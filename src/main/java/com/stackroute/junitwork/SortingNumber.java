66a440135c20a63da6cbdb1919d88ad258c8de15
Neelanshi <neelanshibhatnagar@ymail.com>
2019-07-01 15:59:51 +0530

    solving issues

--- src/main/java/com/stackroute/junitwork/SortingNumber.java
+++ src/main/java/com/stackroute/junitwork/SortingNumber.java
@@ -4,36 +4,44 @@ package com.stackroute.junitwork;
 public class SortingNumber {
 
 
-    public String sum(int number) {
+    public String sum(int num) {
         int sum = 0;
         int count = 0;
-        int temp = number;
-        while (number > 0) {
-            number = number / 10;
+        int temp = num;
+
+// Calculating the length of number
+        while (num > 0) {
+            num = num / 10;
             count++;
         }
-        int array[] = new int[count];
+        int arr[] = new int[count];
+
+// put the number into array
         for (int i = 0; i < count; i++) {
             if (temp % 10 > 0) {
-                array[i] = temp % 10;
+                arr[i] = temp % 10;
                 temp = temp / 10;
 
             }
         }
+// reverse the number
         for (int i = 0; i < count; i++) {
             for (int j = i + 1; j < count; j++) {
-                if (array[i] < array[j]) {
-                    temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+                if (arr[i] < arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
+// adding even number values.
         for (int i = 0; i < count; i++) {
             System.out.print(array[i]);
-            if ((array[i]) % 2 == 0)
-                sum = sum + array[i];
+            if ((arr[i]) % 2 == 0)
+                sum = sum + arr[i];
         }
+
+//checking if sum is greater or smaller than 15
         if (sum > 15) {
            return ("True");
         } else {
